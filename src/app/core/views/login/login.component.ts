d804875d433c5d57beb713c13133753de146f672
Daniel Vargas <Antonio.vargasrosales@gmail.com>
2024-01-27 11:45:59 -0600

    Chore: add singup component to login view

--- src/app/core/views/login/login.component.ts
+++ src/app/core/views/login/login.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { SignInComponent } from '@components/sign-up/sign-up.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, SignInComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
